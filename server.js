ec8bea09368ec165f7ae841bd526319481f773c1
Cory Trast <cory_trast@hotmail.com>
2020-10-29 09:04:30 -0500

    add server

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+
+
+app.listen(PORT, () => {
+    console.log(`Project 2 server is running on port ${PORT}`);
+  });
